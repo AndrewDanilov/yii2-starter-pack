752f34585f7ab4d81262d5f763537f9f36cfcfda
andrey <6097033725>
2019-08-15 11:24:43 +0300

    Misc

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,6 @@ class AppAsset extends AssetBundle
 	public $basePath = '@webroot';
 	public $baseUrl = '@web';
 	public $css = [
-		'/css/jquery.fancybox.min.css',
 		'/css/jquery.mmenu.all.css',
 		'/css/select2.min.css',
 		'/css/swiper.min.css',
@@ -20,7 +19,6 @@ class AppAsset extends AssetBundle
 		'/css/user.custom.css',
 	];
 	public $js = [
-		'/js/jquery.fancybox.min.js',
 		'/js/jquery.mmenu.all.js',
 		'/js/placeholders.min.js',
 		'/js/select2.min.js',
