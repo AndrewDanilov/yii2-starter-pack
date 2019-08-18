fd62145c6fdbddf7c25bece00fb519aedcdb4edb
andrey <6097033725>
2019-08-18 12:56:28 +0300

    Misc

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -1,20 +1,31 @@
 <?php
-return [
-    'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
-        ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
-    ],
+$config = [
+	'components' => [
+		'db' => [
+			'class' => 'yii\db\Connection',
+			'dsn' => 'mysql:host=localhost;dbname=dbname',
+			'username' => 'username',
+			'password' => 'password',
+			'charset' => 'utf8',
+			'enableSchemaCache' => true,
+			'attributes' => [
+				PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));",
+			],
+		],
+	],
 ];
+
+if (YII_ENV_DEV) {
+	$config['bootstrap'][] = 'debug';
+	$config['modules']['debug'] = [
+		'class' => 'yii\debug\Module',
+		'allowedIPs' => ['127.0.0.1', '::1', '192.168.0.*', '192.168.1.*'],
+	];
+	$config['bootstrap'][] = 'gii';
+	$config['modules']['gii'] = [
+		'class' => 'yii\gii\Module',
+		'allowedIPs' => ['127.0.0.1', '::1', '192.168.0.*', '192.168.1.*'],
+	];
+}
+
+return $config;
