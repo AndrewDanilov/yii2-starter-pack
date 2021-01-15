47a983e4e5e7db5f81f04ec20b7b816cc56d58b2
andrey <6097033725>
2021-01-15 03:36:23 +0300

    update

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -4,42 +4,30 @@ use yii\db\Migration;
 
 class m130524_201442_init extends Migration
 {
-    public function up()
-    {
-        $tableOptions = null;
-        if ($this->db->driverName === 'mysql') {
-             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-        }
+	public function up()
+	{
+		$tableOptions = null;
+		if ($this->db->driverName === 'mysql') {
+			// http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+			$tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+		}
 
-        $this->createTable('{{%user}}', [
-            'id' => $this->primaryKey(),
-	        'name' => $this->string()->notNull(),
-            'email' => $this->string()->notNull()->unique(),
-	        'email_confirmed' => $this->tinyInteger(1)->unsigned()->notNull()->defaultValue(0),
-            'auth_key' => $this->string(32)->notNull(),
-            'password_hash' => $this->string()->notNull(),
-            'password_reset_token' => $this->string()->unique(),
-            'status' => $this->smallInteger()->notNull()->defaultValue(10),
-            'created_at' => $this->dateTime(),
-            'updated_at' => $this->dateTime(),
-            'online_at' => $this->dateTime(),
-            'is_admin' => $this->tinyInteger(1)->unsigned()->notNull()->defaultValue(0),
-	        'notify_on' => $this->tinyInteger(1)->unsigned()->notNull()->defaultValue(0),
-        ], $tableOptions);
+		$this->createTable('{{%user}}', [
+			'id' => $this->primaryKey(),
+			'username' => $this->string()->notNull()->unique(),
+			'auth_key' => $this->string(32)->notNull(),
+			'password_hash' => $this->string()->notNull(),
+			'password_reset_token' => $this->string()->unique(),
+			'email' => $this->string()->notNull()->unique(),
 
-        $this->insert('{{%user}}', [
-        	'name' => 'admin',
-        	'email' => 'admin@example.com',
-        	'auth_key' => Yii::$app->security->generateRandomString(),
-        	'password_hash' => Yii::$app->security->generatePasswordHash('admin'),
-	        'status' => 10,
-	        'created_at' => date('Y-m-d H:i:s'),
-	        'is_admin' => 1,
-        ]);
-    }
+			'status' => $this->smallInteger()->notNull()->defaultValue(10),
+			'created_at' => $this->integer()->notNull(),
+			'updated_at' => $this->integer()->notNull(),
+		], $tableOptions);
+	}
 
-    public function down()
-    {
-        $this->dropTable('{{%user}}');
-    }
+	public function down()
+	{
+		$this->dropTable('{{%user}}');
+	}
 }
