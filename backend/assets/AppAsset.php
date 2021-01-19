d448676a4d8b8109b3d7537268fb3827700f699b
andrey <6097033725>
2021-01-19 22:15:38 +0300

    update

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -3,10 +3,6 @@ namespace backend\assets;
 
 use yii\web\AssetBundle;
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -3,18 +3,15 @@ namespace frontend\assets;
 
 use yii\web\AssetBundle;
 
-/**
- * Main frontend application asset bundle.
- */
 class AppAsset extends AssetBundle
 {
 	public $basePath = '@webroot';
 	public $baseUrl = '@web';
 	public $css = [
-		'/css/site.css',
+		'css/site.css',
 	];
 	public $js = [
-		'/js/script.js',
+		'js/script.js',
 	];
 	public $depends = [
 		'yii\web\JqueryAsset',
